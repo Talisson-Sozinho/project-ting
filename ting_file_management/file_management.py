1f69adba9507205a17266e5a5b6b47d68644c9f0
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-27 00:01:27 -0300

    requisito 02 - feita a função "txt_importer"

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+from os import path
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    extension = path.splitext(path_file)[1]
+
+    if extension != '.txt':
+        return print('Formato inválido', file=sys.stderr)
+
+    try:
+        with open(path_file) as file:
+            return [row for row in file.read().split('\n')]
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
