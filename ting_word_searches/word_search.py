27dc11daaecf034c42d659bdcc50cc0f23844624
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-27 16:37:15 -0300

    requisito 07 e 08 - funções "exists_word" e "search_by_word"

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,47 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = []
+
+    for position_on_queue in range(len(instance)):
+        file = instance.search(position_on_queue)
+
+        possible_occurrences = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for row_index, row in enumerate(file["linhas_do_arquivo"], 1):
+            if word.lower() in row.lower():
+                possible_occurrences["ocorrencias"].append({
+                    "linha": row_index
+                })
+
+        if possible_occurrences["ocorrencias"]:
+            response.append(possible_occurrences)
+
+    return response
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = []
+
+    for position_on_queue in range(len(instance)):
+        file = instance.search(position_on_queue)
+
+        possible_occurrences = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for row_index, row in enumerate(file["linhas_do_arquivo"], 1):
+            if word.lower() in row.lower():
+                possible_occurrences["ocorrencias"].append({
+                    "linha": row_index,
+                    "conteudo": row
+                })
+
+        if possible_occurrences["ocorrencias"]:
+            response.append(possible_occurrences)
+
+    return response
