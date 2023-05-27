0677c65b0e8dbd57efe682fbeb21f2a76546af87
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-27 00:31:08 -0300

    requisito 03 ao 05 - funções "process", "remove" e "file_metadata"

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,39 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return
+
+    txt_rows_list = txt_importer(path_file)
+
+    file_for_process = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_rows_list),
+        "linhas_do_arquivo": txt_rows_list
+    }
+
+    instance.enqueue(file_for_process)
+
+    print(file_for_process)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    response = instance.dequeue()
+
+    if response is None:
+        return print('Não há elementos')
+
+    path = response['nome_do_arquivo']
+
+    return print(f'Arquivo {path} removido com sucesso')
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        response = instance.search(position)
+        print(response)
+    except IndexError:
+        print('Posição inválida', file=sys.stderr)
